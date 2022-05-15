4dc380276451958674d6112adad615c8963b728d
obinna-dev <100476375+obinna-dev@users.noreply.github.com>
2022-05-15 19:01:25 +0100

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
 import React, { useEffect, useState } from "react"
 import "../src/styles.css"
 
-/**
- * Challenge:
- * 
- * 1. Create state to hold the current value of the countdown timer.
- *    Display this time in the "Time Remaining" header
- * 
- * 2. Set up an effect that runs every time the `timeRemaining` changes
- *    The effect should wait 1 second, then decrement the `timeRemaining` by 1
- * 
- *    Hint: use `setTimeout` instead of `setInterval`. This will help you avoid
- *    a lot of extra work.
- * 
- *    Warning: there will be a bug in this, but we'll tackle that next
- * 
- * 3. Make it so the effect won't run if the time is already at 0
- */
-
 export default function App() {
 
     const [text, setText] = useState("")
-    const [timeRemaining, setTimeRemaining] = useState(5)
+    const [timeRemaining, setTimeRemaining] = useState(10)
 
     useEffect(()=>{
+        if (timeRemaining > 0) {
         setTimeout(()=>{
-            setTimeRemaining(prevState => (prevState > 0 ? prevState - 1 : prevState))
+            setTimeRemaining(prevState => (prevState - 1))
         }, 1000)
-    }, [timeRemaining])
+    }}, [timeRemaining])
 
     function handleChange(e) {
         const {value} = e.target
