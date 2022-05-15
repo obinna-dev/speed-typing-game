97cfd005f5ad72f0d03d08ddfae416e4e4714cbc
obinna-dev <100476375+obinna-dev@users.noreply.github.com>
2022-05-15 18:56:52 +0100

    Setup timer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import "../src/styles.css"
+
+/**
+ * Challenge:
+ * 
+ * 1. Create state to hold the current value of the countdown timer.
+ *    Display this time in the "Time Remaining" header
+ * 
+ * 2. Set up an effect that runs every time the `timeRemaining` changes
+ *    The effect should wait 1 second, then decrement the `timeRemaining` by 1
+ * 
+ *    Hint: use `setTimeout` instead of `setInterval`. This will help you avoid
+ *    a lot of extra work.
+ * 
+ *    Warning: there will be a bug in this, but we'll tackle that next
+ * 
+ * 3. Make it so the effect won't run if the time is already at 0
+ */
 
 export default function App() {
 
     const [text, setText] = useState("")
+    const [timeRemaining, setTimeRemaining] = useState(5)
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            setTimeRemaining(prevState => (prevState > 0 ? prevState - 1 : prevState))
+        }, 1000)
+    }, [timeRemaining])
 
     function handleChange(e) {
         const {value} = e.target
@@ -15,7 +39,6 @@ export default function App() {
         return wordsArr.filter(word => word !== "").length
     }
 
-    console.log(text)
     return (
         <div>
             <h1>How fast do you type</h1>
@@ -24,7 +47,7 @@ export default function App() {
             placeholder="Start Typing"
             value={text}
             />
-            <h4>Time remaining: ???</h4>
+            <h4>Time remaining: {timeRemaining}</h4>
             <button onClick={()=>{wordCount(text)}}>Start</button>
             <h1>Word count: ???</h1>
         </div>
