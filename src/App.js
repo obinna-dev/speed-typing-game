b8698c71a6b9087576d9ac64229bb82f564d8770
obinna-dev <100476375+obinna-dev@users.noreply.github.com>
2022-05-15 17:49:29 +0100

    Set state to track textarea input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "../src/styles.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+
+    const [textInput, setTextInput] = useState({
+        textarea: ""
+    })
+
+    function handleChange(e) {
+        const {name, value} = e.target
+        setTextInput(prevState => ({[name]:value}))
+    }
+
+    console.log(textInput)
+    return (
+        <div>
+            <h1>How fast do you type</h1>
+            <textarea 
+            onChange={handleChange}
+            placeholder="Start Typing"
+            value={textInput.textarea}
+            name="textArea"
+            />
+            <h4>Time remaining: ???</h4>
+            <button>Start</button>
+            <h1>Word count: ???</h1>
+        </div>
+    )
 }
-
-export default App;
